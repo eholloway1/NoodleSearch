0269bf7b09884eb7e41fd0bc849ce6594d76cb15
Evan.Laptop <eholloway00@gmail.com>
2024-10-27 17:48:58 -0700

    adds materialUI component

--- NoodleSearch.Client/src/search/noodle-search.tsx
+++ NoodleSearch.Client/src/search/noodle-search.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export type googleItem = {
     title: string
@@ -11,10 +13,10 @@ export type imgList = {
 function NoodleSearch() {
     const [query, setQuery] = useState("Noodle");
     const [searchResult, setSearchResult] = useState<imgList>();
-    const api_key = "";
-    const engine_id = "";
-    const url = `https://www.googleapis.com/customsearch/v1?key=${api_key}&cx=${engine_id}f238b1c279426487b&searchType=image&q=${query}water`;
-
+    const api_key = "AIzaSyCd4xJ0yIbfkN19JX4xxeKzE1aenYxqQBA";
+    const engine_id = "f238b1c279426487b";
+    const url = `https://www.googleapis.com/customsearch/v1?key=${api_key}&cx=${engine_id}&searchType=image&q=${query}water`;
+    const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;  
     async function fetcher(url: string) {
         const response = await fetch(url);
         const json = await response.json();
@@ -26,7 +28,9 @@ function NoodleSearch() {
     }, []);
 
   return (
-      <img src={searchResult?.items[0].link} />
+      <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' } }} noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" defaultValue="Noodle it!"/>
+      </Box>
   );
 }
 
