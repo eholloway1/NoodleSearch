f56563e2b20e1f6a9db9dcbb7b4e254e4dd7e45e
Evan Holloway <eholloway00@gmail.com>
2024-10-31 11:55:40 -0700

    adds Search buttun functionality

--- NoodleSearch.Client/src/search/noodle-search.tsx
+++ NoodleSearch.Client/src/search/noodle-search.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import {Button, TextField, Box} from "@mui/material";
 
 export type googleItem = {
     title: string
@@ -11,44 +10,33 @@ export type imgList = {
 }
 
 function NoodleSearch() {
-    const [query, setQuery] = useState("Noodle");
+    const [query, setQuery] = useState("spaghetti");
+    const NoodleIt = "Noodle"
     const [searchResult, setSearchResult] = useState<imgList>();
-<<<<<<< Updated upstream
-    const api_key = "AIzaSyCd4xJ0yIbfkN19JX4xxeKzE1aenYxqQBA";
     const engine_id = "f238b1c279426487b";
-    const url = `https://www.googleapis.com/customsearch/v1?key=${api_key}&cx=${engine_id}&searchType=image&q=${query}water`;
-    const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;  
-=======
-    const engine_id = "f238b1c279426487b";
-    const GOOGLE_API_KEY = "";
-    const url = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${engine_id}&searchType=image&q=${query}water`;
-    
-
->>>>>>> Stashed changes
+    const GOOGLE_API_KEY = "AIzaSyCd4xJ0yIbfkN19JX4xxeKzE1aenYxqQBA";
+    const url = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${engine_id}&searchType=image&q=${NoodleIt}${query}water`;
     async function fetcher(url: string) {
         const response = await fetch(url);
         const json = await response.json();
         setSearchResult(json);
     }
-
+    
     useEffect(() => {
         fetcher(url);
     }, []);
 
-<<<<<<< Updated upstream
-  return (
-      <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' } }} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" defaultValue="Noodle it!"/>
-      </Box>
-=======
     return (
         <div>
         <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' } }} noValidate autoComplete="off" >
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" placeholder="Noodle it!"/>
+            <TextField id="outlined-basic" label="Filled" variant="filled" placeholder="Noodle it!"
+                onChange={(e) => setQuery(e.target.value)}/>
         </Box>
+            <Button variant="contained" onClick={() => {
+                fetcher(url);
+            }}>Search</Button>
             <img src={searchResult?.items[0].link} />
         </div>
->>>>>>> Stashed changes
   );
 }
 
